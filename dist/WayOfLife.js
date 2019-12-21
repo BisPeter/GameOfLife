3ed3ca474347163b0f6d010b9d0c4aa6897459b5
BisPeter <peter.vadle@gmail.com>
2019-12-21 12:53:03 +0100

    fff

--- dist/WayOfLife.js
+++ dist/WayOfLife.js
@@ -118,8 +118,8 @@ function getNeighbourCount(grid, x, y) {
     var sum = 0;
     for (var i = -1; i < 2; i++) {
         for (var j = -1; j < 2; j++) {
-            var cols = (y + i + this.column) % this.column;
-            var rows = (x + j + this.row) % this.row;
+            var cols = (y + i + column) % this.column;
+            var rows = (x + j + row) % this.row;
             if (grid[rows][cols].style.backgroundColor == aliveColor_1) {
                 sum++;
             }
